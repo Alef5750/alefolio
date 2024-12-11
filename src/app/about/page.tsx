e69b003e1e78443328892156cc7bfcf298cec650
Alef Lewitt <alewitt@gmail.com>
2024-12-11 16:11:01 +0200

    about me text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const AboutPage = () => {
-  return <h1>AboutPage</h1>;
+  return (
+    <section className="flex flex-col gap-6 items-center">
+      <h1 className="text-6xl">About Me</h1>
+      <p className="max-w-3xl text-3xl">
+        I build tools with a purpose: to make life simpler, more productive, and
+        focused on what truly matters. Whether it's crafting seamless web
+        applications or designing user-friendly interfaces, my goal is to
+        empower people to spend less time on tasks and more time with loved
+        ones, pursuing their values and passions. My work is driven by
+        creativity, precision, and the belief that technology should enhance—not
+        complicate—our lives. Let’s shape a digital future that prioritizes
+        people and meaningful connections.
+      </p>
+    </section>
+  );
 };
 
 export default AboutPage;
